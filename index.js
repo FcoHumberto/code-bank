4d61b29fe70cb3af71835dd7c81458c2777358e2
Francisco Humberto <fcohrneto@gmail.com>
2026-09-08 14:28:05 -0300

    feat: adicionar botoes de execucao de funcao

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const button = document.querySelector('button');
+const iniciar = document.querySelector('button');
 
-button.addEventListener('click', () => window.location.href = './src/app.html');
+iniciar.addEventListener('click', () =>{
+    function greet() {
+        const username = window.prompt('Digite seu nome:');
+        
+        if (!username){
+            if(username !== ''){
+                return window.alert(`Operação cancelada.`);
+            }
+
+            window.location.href = './src/app.html';
+            return window.alert(`Olá, seja bem-vindo.`);
+
+        } else{
+            window.alert(`Olá, ${username}! Seja bem-vindo.`);
+            window.location.href = './src/app.html';
+        }
+    }
+
+    greet();
+});
